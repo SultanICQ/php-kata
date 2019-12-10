6f1b5344f0d4e4cd84e127db731ab831c052d070
David Garcia Latorre <dgarcia@seocom.agency>
2019-12-10 19:48:36 +0100

    WIP

---
+++ tests/GameTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test_canary()
+    {
+        $this->assertEquals(true, true);
+    }
+}
