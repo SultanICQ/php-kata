89b51d8fb34a441b59d567598859ed041e032518
David Garcia Latorre <dgarcia@seocom.agency>
2019-12-10 19:14:48 +0100

    PHPUnit

---
+++ tests/CanaryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CanaryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test_canary()
+    {
+        $this->assertEquals(true, true);
+    }
+}
